0e205c8e1018695fc7ed9f3cc137e81a495fedcc
steppi <albert_steppi@hms.harvard.edusdfjdifjijs>
2019-03-03 20:46:52 -0500

    Setup and solve LP for cplex solver

--- mae_lasso/cplex_solver.py
+++ mae_lasso/cplex_solver.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 import cplex
 
 
@@ -6,30 +8,41 @@ def cplex_solve(X, y, reg_lambda):
     n, p = X.shape
     model = cplex.Cplex()
     model.objective.set_sense(model.objective.sense.minimize)
-    
+
     # Set display parameters to avoid output being printed to terminal
     model.parameters.barrier.display.set(0)
     model.parameters.simplex.display.set(0)
+
+    # Add variables and objective function
+    obj = [0.0]*(p+1) + [1.0]*n + [reg_lambda]*p
+    model.variables.add(obj=obj)
     
     # Create epsilon constraints
-    rhs = list(y)*2
+    rhs = y.tolist()*2
     senses = 'L'*n + 'G'*n
 
-    top = np.concatenate((np.full((p, 1), 1),
-                          X,
-                          np.full((p, 1), -1)),
-                         axis=1)
-    bottom = np.concatenate((np.full((p, 1), 1),
-                             X,
-                             np.full((p, 1), 1)),
-                            axis=1)
-    constraints = np.concatenate((top, bottom), axis=0)
-    constraints = constraints.tolist()
-                         
-    model.linear_constraints.add(lin_exp=constraints, rhs=rhs, senses=senses)
+    epsilon_upper = [cplex.SparsePair(ind=list(range(p+1)) + [p+i+1],
+                                      val=[1] + row.tolist() + [-1])
+                     for i, row in enumerate(X)]
+    epsilon_lower = [cplex.SparsePair(ind=list(range(p+1)) + [p+i+1],
+                                      val=[1] + row.tolist() + [1])
+                     for i, row in enumerate(X)]
+    constraints = epsilon_upper + epsilon_lower
+    model.linear_constraints.add(lin_expr=constraints, rhs=rhs, senses=senses)
 
     # Create delta constraints
-    rhs = [reg_lambda]*p + [-reg_lambda]*p
-    senses = 'L'*p + 'G'*p
-    
-    
+    rhs = [0]*2*p
+    senses = 'G'*(2*p)
+    delta_upper = [cplex.SparsePair(ind=[1+i, n+p+1+i], val=[1.0, -1.0])
+                   for i in range(p)]
+    delta_lower = [cplex.SparsePair(ind=[1+i, n+p+1+i], val=[-1.0, -1.0])
+                   for i in range(p)]
+    model.linear_constraints.add(lin_expr=delta_upper+delta_lower, rhs=rhs,
+                                 senses=senses)
+
+    # solve problem
+    model.solve()
+    # get coefficients
+    values = model.solution.get_values()
+    return values
+
